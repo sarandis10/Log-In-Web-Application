5b9ab326486774496359b94f5628752c9a865d86
sarandis10 <43549151+sarandis10@users.noreply.github.com>
2019-08-01 19:36:56 +0300

    first commit

---
+++ src/main/java/com/telusko/secureap/SecureApApplication.java
@@ -0,0 +1,13 @@
+package com.telusko.secureap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureApApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureApApplication.class, args);
+	}
+
+}
